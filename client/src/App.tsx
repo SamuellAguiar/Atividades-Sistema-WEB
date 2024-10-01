af8af5cf7a10e7f377968861de8f5c9bc3031dbb
SamuellAguiar <samuellcarlosaguiar@gmail.com>
2024-10-01 02:35:48 -0300

    Init the f

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Menu from './componets/menu/Menu'
+
+function App() {
+
+  return (
+    <>
+      <Menu />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/componets/menu/Menu.tsx
@@ -0,0 +1,8 @@
+const Menu = () => {
+     return (
+          <div>
+               <h1>Aplicação de Doação de Sangue</h1>
+          </div>
+     );
+}
+export default Menu;
